1ebd0ea823b9eca9bf15f7e6a99f0e1049684ca4
tjune1 <130011651+tjune1@users.noreply.github.com>
2023-04-23 19:08:08 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -7,18 +7,15 @@ class Handler implements URLHandler {
     int num = 0;
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Jun's number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
+        if (url.getPath().equals("/increment")) {
             num += 1;
             return String.format("Number incremented!");
         } else {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
+            if (url.getPath().contains("/add-message")) {
                 String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                if (parameters[0].equals("s")) {
+                    return String.format(parameters[1] + "/n");
                 }
             }
             return "404 Not Found!";
@@ -26,7 +23,7 @@ class Handler implements URLHandler {
     }
 }
 
-class NumberServer {
+class StringServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
